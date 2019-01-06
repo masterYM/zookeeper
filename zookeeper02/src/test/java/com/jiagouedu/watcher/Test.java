35ae709f3c934c9628a4e11bb6231892de73135d
592306832@qq.com <592306832@qq.com>
2019-01-06 21:11:50 +0800

    c

--- zookeeper02/src/test/java/com/jiagouedu/watcher/Test.java
+++ zookeeper02/src/test/java/com/jiagouedu/watcher/Test.java
@@ -1,20 +1,4 @@
-package com.jiagouedu.watcher;/* ━━━━━━如来保佑━━━━━━
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　┻　　　┃
- * 　　┗━┓　　　┏━┛
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　　　┣┓
- * 　　　　┃　　　　　　　┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┗┻┛　┗┻┛
- * ━━━━━━永无BUG━━━━━━
- * 图灵学院-悟空老师
- * 以往视频加小乔老师QQ：895900009
- * 悟空老师QQ：245553999
- */
+package com.jiagouedu.watcher;
 
 
 import com.jiagouedu.zkclient.watcher.ZkClientWatcher;
